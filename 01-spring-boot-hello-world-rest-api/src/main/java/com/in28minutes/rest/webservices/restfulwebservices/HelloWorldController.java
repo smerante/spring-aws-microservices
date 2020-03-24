6c1dbaac09b26da1e37aa19ee1ab53b5f63931d7
smerante <merante.s@hotmail.com>
2020-03-24 17:48:05 -0400

    update readme

--- 01-spring-boot-hello-world-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ 01-spring-boot-hello-world-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,24 +1,50 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+class HelloClass {
+	String message;
+
+	public HelloClass(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloClass [message=" + message + "]";
+	}
+}
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
-	public String helloWorld() {
-		return "Hello World";
+	public HelloClass helloWorld() {
+		HelloClass helloClass = new HelloClass("Hello, World!");
+		return helloClass;
 	}
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		//throw new RuntimeException("Some Error has Happened! Contact Support at ***-***");
+		// throw new RuntimeException("Some Error has Happened! Contact Support at
+		// ***-***");
 		return new HelloWorldBean("Hello World - Changed");
 	}
-	
-	///hello-world/path-variable/in28minutes
+
+	/// hello-world/path-variable/in28minutes
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
